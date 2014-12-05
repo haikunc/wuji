7e33b9e204b01c039dbda06cf6463f8d178b8680
haikunc <haikunc@gmail.com>
2014-12-06 07:20:10 +0800

    test

--- cloud/app.js
+++ cloud/app.js
@@ -33,8 +33,8 @@ function querytest(res,seriesID){
 
     var things = new Array();
     for(var i=0; i < tids.length; ++i){
-    	query_thing.get(tids[i],{
-//		success:funtion(res_thing){things.push(res_thing);}
+  	query_thing.get(tids[i],{
+		success:funtion(res_thing){things.push(res_thing);},
 		error:function(res_error){res.render('hello',{message:'error tid'});}
    	 });
     }
