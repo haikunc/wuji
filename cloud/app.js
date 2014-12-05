5706326a9ce75992a130870a0a0cb20686ef27cd
haikunc <haikunc@gmail.com>
2014-12-06 07:55:33 +0800

    test

--- cloud/app.js
+++ cloud/app.js
@@ -34,16 +34,18 @@ function querytest(res,seriesID){
               }
         });
     res.render('hello', { message: tids.length});
+//  for(var i=0; i < tids.length; ++i){
+    for(var i=0; i < tids.length ; ++i){
 /*
-    for(var i=0; i < tids.length; ++i){
     query_thing.get( tids[i], {
               success: function(result) {things.push(result);},
               error: function(error) {res.render('hello', { message: 'Error'});}
         });
+*/
     }
 
-    res.render('hello', { message: things.length});
-*/
+//    res.render('hello', { message: things.length});
+
 
 }
 
