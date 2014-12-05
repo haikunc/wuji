2cd4c21f99591b7d2fcb453cc821d8fbf545fa3c
haikunc <haikunc@gmail.com>
2014-12-06 07:31:41 +0800

    test

--- cloud/app.js
+++ cloud/app.js
@@ -19,9 +19,10 @@ app.get('/hello', function(req, res) {
 function querytest(res,seriesID){
     var query = new AV.Query("series");
     var query_thing = new AV.Query("Things");
+    var tids = null;
     query.get( seriesID, {
               success: function(result) {
-                var tids = result.get("things").split(",");
+                   tids = result.get("things").split(",");
               },
               
               error: function(error) {
