c8633e784c3e1dfa3aadf332d21737057b8d74f5
haikunc <haikunc@gmail.com>
2014-12-05 18:38:06 +0800

    test

--- cloud/app.js
+++ cloud/app.js
@@ -32,7 +32,7 @@ function querytest(res,seriesID){
     var query = new AV.Query("series");
     query.get( seriesID, {
               success: function(result) {
-              	res.render('hello', { message: result.things+result.title});
+              	res.render('hello', { message: result.objectId});
 /*
                 var tids = result.things;
                 var things = tids.split(",");
