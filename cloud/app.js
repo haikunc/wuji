d7dc97436b774ae971f2c00a53ab454784ecc56e
haikunc <haikunc@gmail.com>
2014-12-05 13:12:41 +0800

    test

--- cloud/app.js
+++ cloud/app.js
@@ -2,10 +2,12 @@
 
 var express = require('express');
 var app = express();
+var avosExpressHttpsRedirect = require('avos-express-https-redirect');
 
 // App 全局配置
 app.set('views','cloud/views');   // 设置模板目录
 app.set('view engine', 'ejs');    // 设置 template 引擎
+app.use(avosExpressHttpsRedirect());
 app.use(express.bodyParser());    // 读取请求 body 的中间件
 
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
@@ -13,21 +15,23 @@ app.get('/hello', function(req, res) {
   res.render('hello', { message: 'Congrats, you just set up your app!' });
 });
 
-app.get('/hello2',function(req,res){
+function querytest(res){
   var query = new AV.Query("Things");
-  query.equalTo("name", req.params.movie);
+  query.equalTo("name", "duoduo");
   query.find({
     success: function(results) {
-      var sum = 0;
-      for (var i = 0; i < results.length; ++i) {
-      	res.success(results[i].get("name"));
-      }
+  	res.render('hello', { message: 'Congrats, duoduo just set up your app!' });
     },
-    error: function() {
-      res.error("Things lookup failed");
+    error: function(error) {
+	console.log(error);
+  	res.render('hello', { message: 'Error!' });
     }
   });
 }
+
+app.get('/hello2',function(req,res){
+	querytest(res);
+});
 
 // 最后，必须有这行代码来使 express 响应 HTTP 请求
 app.listen();
