847e842b7ad465827f117a3e34e94815b4cf4f61
haikunc <haikunc@gmail.com>
2014-12-16 15:03:15 +0800

    test

--- cloud/app.js
+++ cloud/app.js
@@ -7,7 +7,7 @@ var app = express();
 // App 全局配置
 app.set('views','cloud/views');   // 设置模板目录
 app.set('view engine', 'ejs');    // 设置 template 引擎
-app.use(avosExpressHttpsRedirect());
+//app.use(avosExpressHttpsRedirect());
 app.use(express.bodyParser());    // 读取请求 body 的中间件
 
 // 使用 Express 路由 API 服务 /hello 的 HTTP GET 请求
