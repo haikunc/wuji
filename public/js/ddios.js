4347f2e23fb15f2899ac200e0edfd4f80b7aab34
haikunc <haikunc@gmail.com>
2014-12-18 00:24:48 +0800

    ctest

--- public/js/ddios.js
+++ public/js/ddios.js
@@ -123,11 +123,11 @@ function envCallback(env_data, context_data) {
         flag = false;
     } else {
         try {
-	    alert(env_data);
             if(typeof(env_data) != "object" || env_data != "zcyApp") {
                 flag = false;
             } else {
                 flag = true;
+	    	alert(env_data);
             }
         } catch(e) {
             flag = false;
