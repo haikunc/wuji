b54063eb323db5ce94f61658aea2b6d594fc89f0
haikunc <haikunc@gmail.com>
2015-01-18 18:09:17 +0800

    abc

--- cloud/main.js
+++ cloud/main.js
@@ -4,3 +4,18 @@ require("cloud/app.js");
 AV.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+AV.Cloud.define("getTopItems", function(request, response) {
+  console.log("Tag: " + request.params.Tag);
+  console.log("Token: " + request.params.token);
+  var query = new AV.Query("series");
+  query.equalTo(request.params.Tag, 1);
+  query.find({
+    success: function(results) {
+      response.success(results);
+    },
+    error: function() {
+      response.error("movie lookup failed");
+    }
+  });
+});
