9490b58c67a8270ff7cd178775fc5a5a1a22293a
haikunc <haikunc@gmail.com>
2015-01-18 19:44:32 +0800

    bbc

--- cloud/main.js
+++ cloud/main.js
@@ -5,11 +5,14 @@ AV.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
 
-AV.Cloud.define("getTopItems", function(request, response) {
+AV.Cloud.define("mayBeLike", function(request, response) {
   console.log("Tag: " + request.params.Tag);
   console.log("Token: " + request.params.token);
   var query = new AV.Query("series");
   query.equalTo(request.params.Tag, 1);
+  query.descending("createdAt");
+  query.limit(10);
+
   query.find({
     success: function(results) {
       response.success(results);
